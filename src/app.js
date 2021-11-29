dc5cc4ea380df61fba0fb870f3b2a04d8a38d48d
aprutledge <alexprutledge@gmail.com>
2021-11-28 20:54:55 -0600

    add cors config for options request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const mongoURI = process.env.MONGO_URI;
 const express = require('express');
+const winston = require('winston'),
+  expressWinston = require('express-winston');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
@@ -23,11 +25,26 @@ class App {
   }
 
   middlewares() {
+    this.server.use(
+      expressWinston.logger({
+        transports: [new winston.transports.Console()],
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.json()
+        ),
+        meta: false,
+        msg: 'HTTP {{req.method}} {{req.url}}',
+        expressFormat: true,
+        colorize: false,
+      })
+    );
     this.server.use(express.json());
     this.server.use(cookieParser(process.env.COOKIE_SECRET));
     this.server.use(express.urlencoded({ extended: true }));
     const corsOptions = {
-      origin: ['http://localhost:8081'],
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+      credentials: true,
     };
     this.server.use(cors(corsOptions));
     this.server.set('etag', false);
